19cb1ccc7aeab10281db4eceee3ea1036d779f3c
Marek Glenda <mrmglenda@gmail.com>
2022-09-07 08:07:44 +0200

    T

--- myClass.py
+++ myClass.py
@@ -1,26 +1,8 @@
-class main():
-    def __init__(self):
-        self.data = []
-    def add(self,x):
-        self.data.append(x)
-    def remove(self,x):
-        self.data.remove(x)
+def reverse(data):
+    for index in range(len(data)-1, -1, -1):
+        yield data[index]
 
-class alt(main):
-    msg = 'ahoj'
-    def __init__(self):
-        super().__init__()
-        self.add(self)
-    def callit(self):
-        print(self.msg)
-    def add(self,x):
-        super().add(x)
-        self.msg = 'speci'
-
-m = main()
-s = alt()
-
-s.callit()
-print(s.data)
-print(m.data)
-s.callit()
+data = 'amadeus mozartus kontextus'
+r = range(len(data)-1,-1,-1)
+res = list(data[i] for i in r)
+print(res)
